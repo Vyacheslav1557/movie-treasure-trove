6eb6fc2f1855ccfb51d89d7083eef517fdc1889e
Vyacheslav1557 <slavabirin1@yandex.ru>
2023-07-03 15:45:16 +0500

    small changes

--- database/models/db_session.py
+++ database/models/db_session.py
@@ -14,7 +14,7 @@ def global_init(db_file: str = PATH_TO_DB):
     if __factory is not None:
         return
     if not db_file or not db_file.strip():
-        raise Exception("Необходимо указать файл базы данных.")
+        raise Exception("Необходимо указать название файла базы данных.")
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
